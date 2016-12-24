c45429260851ac4c9ac5c370bdbf6b325a27e254
Sergey <mr.haiker@mail.ru>
2016-12-24 19:36:08 +0200

    for of

---
+++ dist/for_of.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// support latest version browsers
+
+var browsers = ['Chrome', 'Firefox', 'Edge', 'Safari', 'Opera'];
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = browsers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var browser = _step.value;
+
+        console.log(browsers);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
+
+console.log(browsers);
---
+++ src/for_of.js
@@ -0,0 +1,9 @@
+// support latest version browsers
+
+let browsers = ['Chrome', 'Firefox', 'Edge', 'Safari', 'Opera'];
+
+for (let browser of browsers) {
+    console.log(browsers);
+}
+
+console.log(browsers);
