287a992b9fe3b18c0e803e98ec9b66770b358aae
Sergey <mr.haiker@mail.ru>
2016-12-25 17:16:56 +0200

    Iterators

---
+++ src/iterators.js
@@ -0,0 +1,44 @@
+let xmen = ['Cyclops', 'Wolverine', 'Rogue'];
+
+console.info('for');
+for (let i = 0; i < xmen.length; i++) {
+    console.log(xmen[i]);
+}
+
+
+console.info('for...in');
+for (let key in xmen) {
+    console.log(xmen[key]);
+}
+
+
+console.info('forEach');
+xmen.forEach(xmen => console.log(xmen));
+
+
+console.info('for...of');
+for (let xman of xmen) {
+    console.log(xman);
+}
+
+// ========
+
+let idGenerator = {
+    [Symbol.iterator]() {
+        let id = 1;
+        return {
+            next() {
+                let value = id > 100 ? undefined : id++;
+                let done = !value;
+
+                return {
+                    value: { value, done }
+                }
+            }
+        };
+    }
+};
+
+for (let id of idGenerator) {
+    console.log(id);
+}
